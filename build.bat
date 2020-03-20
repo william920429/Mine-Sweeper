494f3bd60235e2cb19c997a13d2b82722e592095
william920429 <william920429@gmail.com>
2020-03-21 00:08:16 +0800

    upload previously write

---
+++ build.bat
@@ -0,0 +1,3 @@
+chcp 65001
+g++ -o main.exe main.cpp -std=c++17 -lmingw32 -lSDL2main -lSDL2 -lSDL2_image -lSDL2_ttf -mwindows -I"D:\!亦翔\寫程式\c++ includes\SDL2-2.0.9 + image-2.0.4 + ttf-2.0.15\x86_64-w64-mingw32\include" -L"D:\!亦翔\寫程式\c++ includes\SDL2-2.0.9 + image-2.0.4 + ttf-2.0.15\x86_64-w64-mingw32\lib"
+main.exe
